29a6b92e36a3d9dc91dd3330126032e7b63c18a2
rfygno285 <rfygno285@gmail.com>
2025-12-13 10:21:32 +0800

    First Uploading

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "stockledger-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+self.addEventListener("install", (e)=> {
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate", (e)=> {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", (e)=> {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(()=>cached))
+  );
+});
